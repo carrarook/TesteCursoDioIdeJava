6624c9e87837ffadd39c9fae0f0731ea2834d58b
carrarook <obrunocarraro@gmail.com>
2023-03-07 13:50:41 -0300

    First Commit

---
+++ PrimeiroPrograma/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.moddel.Gato;
+
+public class PrimeiroPrograma {
+		public static void main(String[] args) {
+		
+			Gato gato = new Gato();
+			System.out.println(gato);
+	
+		}
+}
+
+class Livros {
+		private String nome;
+		private String npag;
+}
